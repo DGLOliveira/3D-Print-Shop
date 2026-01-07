3e25e2a086d7b70e735f482407c0af4b2ba16dc4
DGLOliveira <dgloliveira90@gmail.com>
2026-01-07 16:19:49 -0100

    Changed About and FAQ css files to modern standards; Added a few anchor links to FAQ page.

--- src/Pages/FAQ.tsx
+++ src/Pages/FAQ.tsx
@@ -18,7 +18,7 @@ export default function FAQ() {
           </details>
           <details>
             <summary><b>How PLA is Recycled?</b></summary>
-            <p><b>PLA should not be thrown in a landfill. Return instead to any buisness related to 3D printing.</b> There are four methods of disposing of PLA plastic:</p>
+            <p><u style={{color:"red"}}><b>PLA should not be thrown in a landfill or common plastic recycling bin! Return instead to any buisness related to 3D printing.</b></u> There are four methods of disposing of PLA plastic:</p>
               <ul>
                 <b>Recasting</b>
                 <p>PLA is a thermoplastic, this means it can be melted and recast into a reusable form several times before impurities become an issue.</p>
@@ -29,11 +29,11 @@ export default function FAQ() {
               </ul>
               <ul>
                 <b>Chemical</b>
-                <p>Its is also possible to revert PLA back into its previous state of lactic açid using Hydrolisis, and then converted back into PLA.</p>
+                <p>Its is also possible to revert PLA back into its previous state of lactic acid using Hydrolisis, and then converted back into PLA.</p>
               </ul>
               <ul>
                 <b>Incinerated</b>
-                <p>If all else fails, the good news is that since this plastic has no toxic components, it can be safelly incinerated, releasing the same subproducts as in composting.</p>
+                <p>If all else fails, the good news is that since this plastic has no toxic components, it can be safelly incinerated with minimal environmental impact. Note that some versions of PLA may contain additives that may cause minor toxicity issues.</p>
               </ul>
           </details>
       </div>
@@ -42,15 +42,15 @@ export default function FAQ() {
           <h4></h4>
           <details>
             <summary><b>Where are the Designs From?</b></summary>
-            <p>Our designs are both custom made, and sourced for free from public works, available at www.thingiverse.com , the original link for each individual design can be found in its respective page. We use only the ones whose Creative Commons licence allows use for commercial applications.</p>
+            <p>Our designs are both custom made, and sourced for free from public works, available at <a href="https://www.thingiverse.com/" target="_blank">www.thingiverse.com</a> , the original link for each individual design can be found in its respective product page. We use only the ones whose Creative Commons licence allows use for commercial applications.</p>
           </details>
           <details>
             <summary><b>How Can I get new Designs?</b></summary>
-            <p>You can make them with free software, or either buy or request a custom design from a professional designer, or search online for available free models online in www.thingiverse.com .</p>
+            <p>You can make them with any 3D software, such as the free and open source <a href="https://www.blender.org/" target="_blank">Blender</a>, or search online for available free models online in <a href="https://www.thingiverse.com/" target="_blank">www.thingiverse.com</a>, or even buy or request a custom design from a professional designer.</p>
           </details>
           <details>
             <summary><b>Can I use Custom Designs?</b></summary>
-            <p>We currently do not produce custom designs. We instead optimize a few models for production and sale. There are however other shops that supply such service.</p>
+            <p>We currently do not produce custom designs. We instead optimize the production of a few models in order to decrease its price tag. There are however other shops that supply such service, at an increased price tag.</p>
           </details>
         </div>
         <div>
@@ -61,7 +61,7 @@ export default function FAQ() {
           </details>
           <details>
             <summary><b>How do we make a 3D Print?</b></summary>
-            <p>We currently use a <b>FDM</b> Printer, it stands for <em>Fusion Deposit Module</em>, this means it heats a material to its melting point and deposits it small layers. The printer can deposit a layer on top of the previous along a set height, and by repeating the process several times, a 3-Dimentional shape arises.</p>
+            <p>We currently use <b>FDM</b> Printers, it stands for <em>Fusion Deposit Module</em>, this means it heats a material to its melting point and deposits it small layers. The printer can deposit a layer on top of the previous along a set height, and by repeating the process several times, a 3-Dimentional shape arises.</p>
           </details>
       </div>
   </div>
